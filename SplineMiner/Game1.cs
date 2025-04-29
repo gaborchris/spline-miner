209c6b9018632f2102771e9842ffd1bf8054395e
Chris Gabor <chrisgabor@E>
2025-04-28 19:49:48 -0700

    cleanup code and add more test data

--- SplineMiner/Game1.cs
+++ SplineMiner/Game1.cs
@@ -15,18 +15,11 @@ namespace SplineMiner
         private InputManager _inputManager;
         private CartController _player;
         private SplineTrack _track;
-        private int _hoveredPointIndex = -1;
-        private SpriteFont _debugFont;
-        private bool _showDebugInfo = true;
         private UIManager _uiManager;
-        private SpriteFont _uiFont;
+        private DebugManager _debugManager;
+        private MouseInteractionManager _mouseInteractionManager;
+        private bool _useLargeTrack = false;
         
-        // FPS tracking
-        private float _fps;
-        private float _frameCounter;
-        private float _timeSinceLastUpdate;
-        private const float UPDATE_INTERVAL = 0.25f; // Update FPS display every quarter second
-
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -72,22 +65,23 @@ namespace SplineMiner
             _player.Texture = minecartTexture;
             _player.LoadDebugTexture(GraphicsDevice);
 
-            // Create a simple debug font - since we don't have a real font loaded
+            // Load fonts
+            SpriteFont debugFont = null;
             try 
             {
-                _debugFont = Content.Load<SpriteFont>("debug_font");
-                _uiFont = Content.Load<SpriteFont>("debug_font");
+                debugFont = Content.Load<SpriteFont>("debug_font");
             }
             catch 
             {
                 Debug.WriteLine("Fonts not found. Debug text and UI will not be rendered.");
             }
 
-            // Initialize UI manager
-            _uiManager = new UIManager(_uiFont, GraphicsDevice);
+            // Initialize managers
+            _uiManager = new UIManager(debugFont, GraphicsDevice);
+            _debugManager = new DebugManager(debugFont);
             
             // Initialize the track with test data
-            _track = new SplineTrack(TestData.TestTrack.GetTestTrackNodes(), _uiManager);
+            InitializeTrack();
             
             // Load track textures
             _track.LoadContent(GraphicsDevice);
@@ -96,21 +90,20 @@ namespace SplineMiner
             _track.RecalculateArcLength();
         }
 
+        private void InitializeTrack()
+        {
+            var trackNodes = _useLargeTrack 
+                ? TestData.TestTrack.GetLargeTrackNodes() 
+                : TestData.TestTrack.GetSmallTrackNodes();
+            
+            _track = new SplineTrack(trackNodes, _uiManager);
+            _mouseInteractionManager = new MouseInteractionManager(_inputManager, _track);
+        }
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // Update FPS counter
-            _timeSinceLastUpdate += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            _frameCounter++;
-            
-            if (_timeSinceLastUpdate >= UPDATE_INTERVAL)
-            {
-                _fps = _frameCounter / _timeSinceLastUpdate;
-                _frameCounter = 0;
-                _timeSinceLastUpdate = 0;
-            }
 
             // Update input
             _inputManager.Update();
@@ -121,131 +114,47 @@ namespace SplineMiner
             // Update camera
             CameraManager.Instance.Update(gameTime);
             
-            // Handle mouse interaction with control points based on current tool
-            HandleMouseInteraction();
+            // Update mouse interactions
+            _mouseInteractionManager.Update(_uiManager.CurrentTool);
 
             // Update player movement along the track
             _player.Update(gameTime, _track);
 
+            // Update debug manager
+            _debugManager.Update(gameTime);
+
             // Toggle debug info with F1
             if (_inputManager.IsKeyPressed(Keys.F1))
             {
-                _showDebugInfo = !_showDebugInfo;
+                _debugManager.ShowDebugInfo = !_debugManager.ShowDebugInfo;
+            }
+
+            // Toggle between large and small track with F2
+            if (_inputManager.IsKeyPressed(Keys.F2))
+            {
+                _useLargeTrack = !_useLargeTrack;
+                InitializeTrack();
+                _track.LoadContent(GraphicsDevice);
+                _track.RecalculateArcLength();
             }
 
             base.Update(gameTime);
-        }
-        
-        private void HandleMouseInteraction()
-        {
-            // Get the mouse position
-            Vector2 mousePosition = _inputManager.MousePosition;
-            
-            switch (_uiManager.CurrentTool)
-            {
-                case UITool.Track:
-                    
-                    // Handle right-click drag for editing shadow nodes
-                    if (_inputManager.IsRightMousePressed())
-                    {
-                        int pointIndex = _track.GetHoveredPointIndex(mousePosition);
-                        if (pointIndex != -1)
-                        {
-                            _track.SelectPoint(pointIndex);
-                        }
-                    }
-                    else if (_inputManager.IsRightMouseHeld())
-                    {
-                        _track.MoveSelectedPoint(mousePosition);
-                    }
-                    else if (_inputManager.IsRightMouseReleased())
-                    {
-                        _track.ReleaseSelectedPoint();
-                    }
-                    // Handle left-click for placing new points
-                    else if (_inputManager.IsLeftMousePressed())
-                    {
-                        if (!_track.IsHoveringEndpoint)
-                        {
-                            _track.PlaceNode(mousePosition);
-                        }
-                    }
-                    break;
-
-                case UITool.DeleteTrack:
-                    if (_inputManager.IsLeftMousePressed())
-                    {
-                        int pointIndex = _track.GetHoveredPointIndex(mousePosition);
-                        if (pointIndex != -1)
-                        {
-                            _track.DeletePoint(pointIndex);
-                        }
-                    }
-                    break;
-            }
-            
-            // Update hovered point for visual feedback
-            _hoveredPointIndex = _track.GetHoveredPointIndex(mousePosition);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            // Draw game world (affected by camera)
             _spriteBatch.Begin(transformMatrix: CameraManager.Instance.Transform);
-
-            // Draw the track
             _track.Draw(_spriteBatch);
-
-            // Draw debug info for the track
-            if (_showDebugInfo)
-            {
-                _track.DrawDebugInfo(_spriteBatch, _player.CurrentDistance, _player.DebugTexture);
-            }
-
-            // Draw the player
             _player.Draw(_spriteBatch);
-
-            // Draw UI (UI should not be affected by camera)
             _spriteBatch.End();
+
+            // Draw UI (not affected by camera)
             _spriteBatch.Begin();
             _uiManager.Draw(_spriteBatch);
-
-            // Draw debug info
-            if (_showDebugInfo)
-            {
-                // Create a simple debug background
-                Texture2D debugBg = new Texture2D(GraphicsDevice, 1, 1);
-                debugBg.SetData(new[] { Color.Black * 0.7f });
-                
-                string[] debugInfo = {
-                    "Controls:",
-                    "T: Test cart movement",
-                    "V: Visualize equally spaced points",
-                    "F1: Toggle debug info",
-                    "Left/Right: Move cart",
-                    $"Current Tool: {_uiManager.CurrentTool}",
-                    $"FPS: {_fps:F5}"
-                };
-
-                // Calculate starting Y position at bottom of screen
-                float yPos = GraphicsDevice.Viewport.Height - (debugInfo.Length * 25) - 10;
-                
-                foreach (string line in debugInfo)
-                {
-                    // Simple text background since we don't have a font
-                    _spriteBatch.Draw(debugBg, new Rectangle(10, (int)yPos, 250, 20), Color.White);
-                    
-                    // If we do have the font, use it, otherwise just advance position
-                    if (_debugFont != null)
-                    {
-                        _spriteBatch.DrawString(_debugFont, line, new Vector2(15, yPos), Color.White);
-                    }
-                    
-                    yPos += 25;
-                }
-            }
-
+            _debugManager.Draw(_spriteBatch, GraphicsDevice, _uiManager.CurrentTool);
             _spriteBatch.End();
 
             base.Draw(gameTime);
